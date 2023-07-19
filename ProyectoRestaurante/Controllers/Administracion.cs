60638ef61c6d8c2a945679c03cdadcd1289d4c15
Anto462 <0anthony0402@gmail.com>
2023-07-19 00:40:06 -0600

    Se adjunta la apgina de administracion, sera un dasboards para los administradores y empleados

---
+++ ProyectoRestaurante/Controllers/Administracion.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoRestaurante.Controllers
+{
+    public class Administracion : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
