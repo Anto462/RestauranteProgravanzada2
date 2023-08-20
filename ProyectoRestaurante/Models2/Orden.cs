df900759b32a5ae2eb4b87f975be00c0bb0954a7
dfalm <105889122+dfalm@users.noreply.github.com>
2023-08-20 15:41:40 -0600

    Cambios a varias vistas

--- ProyectoRestaurante/Models2/Orden.cs
+++ ProyectoRestaurante/Models2/Orden.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MessagePack;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace ProyectoRestaurante.Models2
 {
@@ -10,12 +12,22 @@ namespace ProyectoRestaurante.Models2
             DetalleOrdens = new HashSet<DetalleOrden>();
         }
 
+        [DisplayName("Id Orden")]
         public int IdOrden { get; set; }
+
+        [DisplayName("Id")]
         public string Id { get; set; } = null!;
+
+        [DisplayName("Mesa")]
         public int IdMesa { get; set; }
+
+        [DisplayName("Fecha")]
         public DateTime FechaOrden { get; set; }
 
+        [DisplayName("# Mesa")]
         public virtual Mesa IdMesaNavigation { get; set; } = null!;
+
+        [DisplayName("Id Usuario")]
         public virtual AspNetUser IdNavigation { get; set; } = null!;
         public virtual ICollection<DetalleOrden> DetalleOrdens { get; set; }
     }
